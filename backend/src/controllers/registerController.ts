4b17097234612eb285e9ec45128fe278f987dfc4
Fillip14 <fillip.apol@gmail.com>
2025-02-13 18:05:22 -0300

    Adicionado registro para empresas. Adicionados mais campos no registro

--- backend/src/controllers/registerController.ts
+++ backend/src/controllers/registerController.ts
@@ -1,12 +1,24 @@
 import { supabase } from '../database/supabase';
 
 export const UserService = {
-  async register(username: string, password: string) {
+  async register(
+    email: string,
+    name: string,
+    cpf: string,
+    country: string,
+    state: string,
+    city: string,
+    neighborhood: string,
+    street: string,
+    number: string,
+    zipcode: string,
+    password: string
+  ) {
     // Verifica se o usuário já existe
     const { data: existingUser, error: findError } = await supabase
       .from('users')
       .select('*')
-      .eq('username', username)
+      .eq('email', email)
       .single();
 
     if (existingUser) {
@@ -16,12 +28,79 @@ export const UserService = {
     // Insere o novo usuário
     const { data: newUser, error: insertError } = await supabase
       .from('users')
-      .insert([{ username, password }])
+      .insert([
+        {
+          email,
+          name,
+          cpf,
+          country,
+          state,
+          city,
+          neighborhood,
+          street,
+          number,
+          zipcode,
+          password,
+        },
+      ])
       .select();
 
     if (insertError) {
       throw new Error('Erro ao cadastrar usuário.');
     }
-    return newUser[0].username;
+    return newUser[0].name;
+  },
+};
+
+export const CompanyService = {
+  async register(
+    companyEmail: string,
+    companyName: string,
+    cnpj: string,
+    country: string,
+    state: string,
+    city: string,
+    neighborhood: string,
+    street: string,
+    number: string,
+    zipcode: string,
+    password: string
+  ) {
+    // Verifica se o usuário já existe
+    const { data: existingCompany, error: findError } = await supabase
+      .from('companies')
+      .select('*')
+      .eq('companyName', companyName)
+      .single();
+
+    if (existingCompany) {
+      throw new Error('Empresa já existe.');
+    }
+
+    // Insere o novo usuário
+    const { data: newCompany, error: insertError } = await supabase
+      .from('companies')
+      .insert([
+        {
+          companyEmail,
+          companyName,
+          cnpj,
+          country,
+          state,
+          city,
+          neighborhood,
+          street,
+          number,
+          zipcode,
+          password,
+        },
+      ])
+      .select();
+
+    if (insertError) {
+      console.log(insertError);
+      throw new Error('Erro ao cadastrar empresa.');
+    }
+    return newCompany[0].companyName;
   },
 };
--- backend/src/routes/routes.ts
+++ backend/src/routes/routes.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { UserService } from '../controllers/registerController';
+import { UserService, CompanyService } from '../controllers/registerController';
 const router = express.Router();
 
 router.get('/', (req, res) => {
@@ -10,12 +10,71 @@ router.get('/ajuda', (req, res) => {
   res.send('Ajuda');
 });
 
-router.post('/register', async (req, res) => {
-  const { username, password } = req.body;
+router.post('/register/user', async (req, res) => {
+  const {
+    email,
+    name,
+    cpf,
+    country,
+    state,
+    city,
+    neighborhood,
+    street,
+    number,
+    zipcode,
+    password,
+  } = req.body;
 
   try {
-    const user = await UserService.register(username, password);
+    const user = await UserService.register(
+      email,
+      name,
+      cpf,
+      country,
+      state,
+      city,
+      neighborhood,
+      street,
+      number,
+      zipcode,
+      password
+    );
     res.status(201).json({ user });
+  } catch (error: any) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+router.post('/register/company', async (req, res) => {
+  const {
+    companyEmail,
+    companyName,
+    cnpj,
+    country,
+    state,
+    city,
+    neighborhood,
+    street,
+    number,
+    zipcode,
+    password,
+  } = req.body;
+
+  try {
+    const company = await CompanyService.register(
+      companyEmail,
+      companyName,
+      cnpj,
+      country,
+      state,
+      city,
+      neighborhood,
+      street,
+      number,
+      zipcode,
+      password
+    );
+    res.status(201).json({ company });
   } catch (error: any) {
     res.status(400).json({ error: error.message });
   }
