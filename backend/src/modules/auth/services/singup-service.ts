324eabbb167d1ca9aa23d7ebd2ffce0a46050eb7
Fillip14 <fillip.apol@gmail.com>
2025-02-16 09:21:43 -0300

    Adicionado criptografia de passwrod com bcrypt

--- backend/src/modules/auth/services/singup-service.ts
+++ backend/src/modules/auth/services/singup-service.ts
@@ -1,8 +1,11 @@
 import { userDoesNotExist, create } from '../repositories/signup-repository';
 import { Client, Company } from '../schemas/signup-schema';
+import bcrypt from 'bcrypt';
 
 export const signUpService = {
   async register(userData: Client | Company): Promise<string> {
+    userData.password = await bcrypt.hash(userData.password, 10);
+
     const existingUser = await userDoesNotExist(userData);
 
     if (existingUser) throw new Error(`Usuário já existe. ${userData.type}`);
