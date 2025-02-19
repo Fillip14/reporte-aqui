291a3064652c90b5d8aa8b400784f4bff885db24
Fillip14 <fillip.apol@gmail.com>
2025-02-19 18:14:36 -0300

    Pequenas alteracoes.

--- backend/src/modules/auth/schemas/sign-in.schema.ts
+++ backend/src/modules/auth/schemas/sign-in.schema.ts
@@ -1,8 +1,9 @@
 import { z } from 'zod';
+import { UserType } from '../constants/auth.constants';
 
 export const signInSchema = z.object({
   id: z.string(),
-  type: z.string(),
+  type: z.nativeEnum(UserType),
   email: z.string().email(),
   password: z.string().min(6),
 });
