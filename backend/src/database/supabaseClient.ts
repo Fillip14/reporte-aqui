72f824e269a695c65df21eec2f824d4ccddf6bcb
Fillip14 <fillip.apol@gmail.com>
2025-04-01 17:21:44 -0300

    Alterado a key para conectar com o supabase

--- backend/src/database/supabaseClient.ts
+++ backend/src/database/supabaseClient.ts
@@ -4,6 +4,6 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const supabaseUrl = process.env.SUPABASE_URL as string;
-const supabaseKey = process.env.SUPABASE_KEY as string;
+const supabaseKey = process.env.SUPABASE_SERVICE_KEY as string;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
