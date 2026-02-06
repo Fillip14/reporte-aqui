aab4647d294f055642bc8879d6cf9faf291c7b76
Fillip <fillip.apol@gmail.com>
2026-02-06 18:06:07 -0300

    Adicionado nova rota para checar documento, retirado da rota signup.

--- backend/src/modules/auth/routes/auth.routes.ts
+++ backend/src/modules/auth/routes/auth.routes.ts
@@ -2,10 +2,12 @@ import { Router } from 'express';
 import { registerController } from '../controllers/sign-up.controller';
 import { loginController } from '../controllers/sign-in.controller';
 import { logoutController } from '../controllers/logout.controller';
+import { checkDocumentController } from '../controllers/check-document.controller';
 
 const routesAuth = Router();
 const AUTH_BASE_PATH = '/auth';
 
+routesAuth.post(AUTH_BASE_PATH + '/check-document', checkDocumentController);
 routesAuth.post(AUTH_BASE_PATH + '/signup', registerController);
 routesAuth.post(AUTH_BASE_PATH + '/signin', loginController);
 routesAuth.post(AUTH_BASE_PATH + '/logout', logoutController);
