fd0aa781c56dc3030262b35f58474cdc708c8a68
Fillip <fillip.apol@gmail.com>
2026-02-07 08:15:35 -0300

    Atualizado nome da constante user_id.

--- backend/src/modules/profile/service/profile.service.ts
+++ backend/src/modules/profile/service/profile.service.ts
@@ -5,8 +5,8 @@ export const getProfileService = async (userData: ProfileData) => {
   return await findUserByID(userData);
 };
 
-export const patchProfileService = async (userData: ProfileUpdate, userID: string) => {
-  return await patchUser(userData, userID);
+export const patchProfileService = async (userData: ProfileUpdate, user_id: string) => {
+  return await patchUser(userData, user_id);
 };
 
 export const deleteProfileService = async (userData: ProfileData) => {
