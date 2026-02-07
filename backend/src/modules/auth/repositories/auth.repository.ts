81b7d486a6d8a2a0acd7774f96595cdb1753efca
Fillip <fillip.apol@gmail.com>
2026-02-07 07:43:31 -0300

    Atualizado auth.repository para lidar com o novo metodo create.

--- backend/src/modules/auth/repositories/auth.repository.ts
+++ backend/src/modules/auth/repositories/auth.repository.ts
@@ -1,30 +1,24 @@
 import { supabase } from '../../../database/supabaseClient';
 import { SignIn } from '../schemas/sign-in.schema';
 import { DocumentOrEmail, SignUp } from '../schemas/sign-up.schema';
-import {
-  TABLE_AUTH,
-  TABLE_USERS,
-  TABLE_PROFILES,
-  COLUMN_USER_ID,
-  COLUMN_UUID,
-} from '../../../constants/database.constants';
+import { Table, Column, AccountStatus } from '../../../constants/database.constants';
 
 export const findUser = async (itemToSearch: SignIn) => {
   const { data: authUser, error: findError } = await supabase
-    .from(TABLE_AUTH)
-    .select(`${COLUMN_USER_ID}, password_hash`)
-    .eq('provider', itemToSearch.provider)
-    .eq('provider_uid', itemToSearch.providerUid)
+    .from(Table.AUTH)
+    .select(`${Column.USER_ID}, password_hash`)
+    .eq(Column.PROVIDER, itemToSearch.provider)
+    .eq(Column.PROVIDER_UID, itemToSearch.providerUid)
     .single();
 
   if (findError) throw new Error('Erro ao pesquisar cadastro.');
 
-  const userID = authUser[COLUMN_USER_ID];
+  const userID = authUser[Column.USER_ID];
 
   const { data: dataUser, error: userError } = await supabase
-    .from(TABLE_USERS)
+    .from(Table.USERS)
     .select('type')
-    .eq(COLUMN_UUID, userID)
+    .eq(Column.UUID, userID)
     .single();
 
   if (userError) throw new Error('Erro ao pesquisar cadastro.');
@@ -36,61 +30,84 @@ export const findRegisteredUser = async (
   value: DocumentOrEmail['value'],
   field: DocumentOrEmail['field'],
 ) => {
-  const { count, error } = await supabase
-    .from(TABLE_USERS)
-    .select('*', { count: 'exact', head: true })
+  const { data: usersData, error: usersError } = await supabase
+    .from(Table.USERS)
+    .select('*')
     .eq(field, value);
 
-  if (error) throw new Error(`Erro ao verificar ${field}.`);
+  if (usersError) throw new Error(`Erro ao verificar ${field}.`);
 
-  return (count ?? 0) > 0;
+  return usersData;
 };
 
-export const create = async (userData: SignUp): Promise<string> => {
-  const status = 'active';
-  const { data: newUser, error: userInsertError } = await supabase
-    .from(TABLE_USERS)
-    .insert({
-      type: userData.type,
-      status: status,
-      email: userData.email,
-      document: userData.document,
-    })
-    .select(COLUMN_UUID)
-    .single();
+export const create = async (userData: SignUp, existingUserId?: string): Promise<string> => {
+  let userID: string;
+  try {
+    if (existingUserId) {
+      const { error: updateUserError } = await supabase
+        .from(Table.USERS)
+        .update({
+          type: userData.type,
+          status: AccountStatus.ACTIVE,
+          email: userData.email,
+          document: userData.document,
+        })
+        .eq(Column.UUID, existingUserId);
 
-  if (userInsertError) throw new Error('Erro ao cadastrar no users.');
+      if (updateUserError) {
+        throw new Error('Erro ao restaurar usuário.');
+      }
 
-  const userID = newUser.uuid;
+      userID = existingUserId;
+    } else {
+      const { data: newUser, error: userInsertError } = await supabase
+        .from(Table.USERS)
+        .insert({
+          type: userData.type,
+          status: AccountStatus.ACTIVE,
+          email: userData.email,
+          document: userData.document,
+        })
+        .select(Column.UUID)
+        .single();
 
-  const { data: newAuthUser, error: authInsertError } = await supabase
-    .from(TABLE_AUTH)
-    .insert({
-      user_id: userID,
-      provider: userData.provider,
-      provider_uid: userData.providerUid,
-      password_hash: userData.password,
-    })
-    .select();
+      if (userInsertError) throw new Error('Erro ao cadastrar no users.');
 
-  if (authInsertError) throw new Error('Erro ao cadastrar no auth.');
+      userID = newUser.uuid;
+    }
 
-  const { data: newProfile, error: profileInsertError } = await supabase
-    .from(TABLE_PROFILES)
-    .insert({
-      user_id: userID,
-      name: userData.name,
-      country: userData.country,
-      state: userData.state,
-      city: userData.city,
-      neighborhood: userData.neighborhood,
-      street: userData.street,
-      number: userData.number,
-      zipcode: userData.zipcode,
-    })
-    .select();
+    const { error: authInsertError } = await supabase
+      .from(Table.AUTH)
+      .insert({
+        user_id: userID,
+        provider: userData.provider,
+        provider_uid: userData.providerUid,
+        password_hash: userData.password,
+      })
+      .select();
 
-  if (profileInsertError) throw new Error('Erro ao cadastrar no profile.');
+    if (authInsertError) throw new Error('Erro ao cadastrar no auth.');
 
-  return newProfile[0].name;
+    const { data: newProfile, error: profileInsertError } = await supabase
+      .from(Table.PROFILES)
+      .insert({
+        user_id: userID,
+        name: userData.name,
+        country: userData.country,
+        state: userData.state,
+        city: userData.city,
+        neighborhood: userData.neighborhood,
+        street: userData.street,
+        number: userData.number,
+        zipcode: userData.zipcode,
+      })
+      .select();
+
+    if (profileInsertError) throw new Error('Erro ao cadastrar no profile.');
+    return newProfile[0].userID;
+  } catch (error) {
+    //SOFT DELETE
+    console.log(error);
+    throw new Error('Erro ao cadastrar no auth.');
+  }
 };
