2635bb353cbb159c4b78f380a9b0c867931093c9
Fillip <fillip.apol@gmail.com>
2026-02-07 08:14:18 -0300

    Pequena atualizacao no middleware validate.

--- backend/src/middlewares/validate-schema.middleware.ts
+++ backend/src/middlewares/validate-schema.middleware.ts
@@ -3,9 +3,16 @@ import { Request, Response, NextFunction } from 'express';
 import { AppError } from '../errors/AppError';
 import { HttpStatus } from '../constants/api.constants';
 
-export const validate = <T extends ZodSchema>(schema: T) => {
+type DataSource = (req: Request, res: Response) => any;
+
+export const validate = <T extends ZodSchema>(
+  schema: T,
+  dataSource: DataSource = (req, _res) => req.body, // default: req.body
+) => {
   return (req: Request, res: Response, next: NextFunction) => {
-    const result = schema.safeParse(req.body);
+    const data = dataSource(req, res);
+    const result = schema.safeParse(data);
+
     if (!result.success) {
       throw new AppError('Dados inválidos', HttpStatus.BAD_REQUEST, {
         errors: result.error.flatten().fieldErrors,
