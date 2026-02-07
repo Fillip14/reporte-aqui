d28622fc53cdba8b2d0684b9f03d0650159ea914
Fillip <fillip.apol@gmail.com>
2026-02-07 08:14:03 -0300

    Adicionado middleware validate.

--- backend/src/modules/profile/routes/profile.routes.ts
+++ backend/src/modules/profile/routes/profile.routes.ts
@@ -1,15 +1,28 @@
 import { Router } from 'express';
-import { authMiddleware } from '../../../middlewares/middleware';
+import { authMiddleware } from '../../../middlewares/auth.middleware';
 import {
   getProfileController,
   patchProfileController,
   deleteProfileController,
 } from '../controller/profile.controller';
+import { validate } from '../../../middlewares/validate-schema.middleware';
+import { profileDataSchema, profileUpdateSchema } from '../schemas/profile.schema';
 
 const routesProfile = Router();
+const PROFILE_BASE_PATH = '/profile';
 
-routesProfile.get('/profile', authMiddleware(), getProfileController);
-routesProfile.patch('/profile', authMiddleware(), patchProfileController);
-routesProfile.delete('/profile', authMiddleware(), deleteProfileController);
+routesProfile.get(
+  PROFILE_BASE_PATH,
+  authMiddleware(),
+  validate(profileDataSchema, (_req, res) => res.locals.user),
+  getProfileController,
+);
+routesProfile.patch(
+  PROFILE_BASE_PATH,
+  authMiddleware(),
+  validate(profileUpdateSchema),
+  patchProfileController,
+);
+routesProfile.delete(PROFILE_BASE_PATH, authMiddleware(), deleteProfileController);
 
 export default routesProfile;
