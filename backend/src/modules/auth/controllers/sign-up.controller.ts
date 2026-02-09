70c95a575561bef5fbfd1707961733838353e5ec
Fillip <fillip.apol@gmail.com>
2026-02-09 09:00:25 -0300

    Adicionado um log que faltava.

--- backend/src/modules/auth/controllers/sign-up.controller.ts
+++ backend/src/modules/auth/controllers/sign-up.controller.ts
@@ -2,12 +2,14 @@ import { HttpStatus } from '../../../constants/api.constants';
 import { registerUserService } from '../services/sing-up.service';
 import express, { Request, Response } from 'express';
 import { asyncHandler } from '../../../utils/asyncHandler';
+import logger from '../../../utils/log/logger';
 
 export const registerController = asyncHandler(async (req: Request, res: Response) => {
   const userDataValidated = res.locals.validated;
 
   const userRegistered = await registerUserService(userDataValidated);
 
+  logger.info(`Usuario registrado com sucesso: ${userRegistered}`);
   return res.status(HttpStatus.CREATED).json({
     success: true,
     userID: userRegistered,
