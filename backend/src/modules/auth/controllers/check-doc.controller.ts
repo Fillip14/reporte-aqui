d96f826aa057c8d67d1de66ca1f5341c9f868c71
Fillip <fillip.apol@gmail.com>
2026-02-09 10:20:32 -0300

    Envolvido check-doc em asynchandler

--- backend/src/modules/auth/controllers/check-doc.controller.ts
+++ backend/src/modules/auth/controllers/check-doc.controller.ts
@@ -2,18 +2,17 @@ import { HttpStatus } from '../../../constants/api.constants';
 import express, { Request, Response } from 'express';
 import { checkDocService } from '../services/check-doc.service';
 import logger from '../../../utils/log/logger';
+import { asyncHandler } from '../../../utils/asyncHandler';
 
-export const checkDocController = async (req: Request, res: Response) => {
+export const checkDocController = asyncHandler(async (req: Request, res: Response) => {
   const { document } = req.body;
   const result = await checkDocService(document);
 
   logger.info(`Documento verificado com sucesso: ${document}`);
-  res
-    .status(HttpStatus.OK)
-    .json({
-      exists: result.exists,
-      status: result.status,
-      suggestedAction: result.suggestedAction,
-    });
+  res.status(HttpStatus.OK).json({
+    exists: result.exists,
+    status: result.status,
+    suggestedAction: result.suggestedAction,
+  });
   return;
-};
+});
