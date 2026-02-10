fa204db3d5302c9b10c2a7e3fea3877aa181e6bd
Fillip <fillip.apol@gmail.com>
2026-02-10 07:45:24 -0300

    Alterado server.ts, adicionado errorMiddleware, adicionado redirecionamento de rota nao existente, adicionado prefixo.

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,18 +1,34 @@
 import express from 'express';
 import routes from './routes/routes';
 import routesAuth from './modules/auth/routes/auth.routes';
-import routesProfile from './modules/profile/routes/profile.routes';
-import routesReports from './modules/individuals/routes/individual-routes';
+import routesReports from './modules/individuals/routes/individual.routes';
 import storageRoutes from './modules/storage/routes/storage.routes';
+import routesCompany from './modules/companies/routes/company-routes';
+import { HttpStatus } from './constants/api.constants';
+import { errorMiddleware } from './middlewares/error.middleware';
+import routesAccount from './modules/account/routes/account.routes';
+
 const cookieParser = require('cookie-parser');
+const API_PREFIX = '/api';
 
 export const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(routesAuth);
-app.use(routesProfile);
-app.use(routesProfile);
-app.use(routesReports);
-app.use(storageRoutes);
-app.use(routes);
+app.use(API_PREFIX, routesAuth);
+app.use(API_PREFIX, routesAccount);
+app.use(API_PREFIX, routesReports);
+app.use(API_PREFIX, storageRoutes);
+app.use(API_PREFIX, routesCompany);
+app.use(API_PREFIX, routes);
+
+app.use(API_PREFIX, (req, res) => {
+  res.status(HttpStatus.NOT_FOUND).json({ message: 'API route not found' });
+});
+
+app.use(errorMiddleware);
+// app.use(express.static(frontendPath));
+
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(frontendPath, 'index.html'));
+// });
